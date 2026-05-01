57acded15a10763925c5b7835b65504edeacb799
marco-valim <marco.antvs@gmail.com>
2026-05-01 11:06:16 -0400

    criado lista de tarefas e barra de progresso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,78 @@
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Input} from '@/components/ui/input'
 import { Separator } from '@base-ui/react'
-import { Plus, List, Check, CircleX  } from 'lucide-react'
+import { Plus, List, Check, CircleX, SquarePen, Trash, ListChecks, Sigma  } from 'lucide-react'
 
 
 const Home = () => {
   return (
     <main className="w-full h-screen bg-gray-400 flex justify-center items-center">
-      <Card className='w-lg p-4'>
-        <div className='flex gap-2'>
+      <Card className='w-lg'>
+        <CardHeader className='flex gap-2'>
           <Input placeholder='Adicionar tarefa' />
           <Button variant="outline" className="cursor-pointer"><Plus />Cadastrar</Button>
+        </CardHeader>
+
+        <CardContent>
+        <Separator className="mb-4 border-b"/>
+
+        <div className='flex gap-2'>          
+          <Badge className="cursor-pointer" variant="default"><List />Todas</Badge>
+          <Badge className="cursor-pointer" variant="outline"><CircleX />Não finalizadas</Badge>
+          <Badge className="cursor-pointer" variant="outline"><Check />Concluídas</Badge>
         </div>
 
-        <Separator />
+        <div className='mt-4 border-b'>
 
-        <div className='flex gap-2'>
-          <Badge className="cursor-pointer"><List />Todas</Badge>
-          <Badge className="cursor-pointer"><CircleX />Não finalizadas</Badge>
-          <Badge className="cursor-pointer"><Check />Concluídas</Badge>
+          <div className='h-14 flex justify-between items-center border-t'>
+            <div className='w-1 h-full bg-green-300'></div>
+            <p className='flex-1 px-2 text-sm'>Estudar React</p>
+            <div className='flex gap-2 items-center'>
+              <SquarePen size={16} className='cursor-pointer' />
+              <Trash size={16} className='cursor-pointer' />
+            </div>
+          </div>
+
+          <div className='h-14 flex justify-between items-center border-t'>
+            <div className='w-1 h-full bg-green-300'></div>
+            <p className='flex-1 px-2 text-sm'>Estudar React</p>
+            <div className='flex gap-2 items-center'>
+              <SquarePen size={16} className='cursor-pointer' />
+              <Trash size={16} className='cursor-pointer' />
+            </div>
+          </div>
+
+          <div className='h-14 flex justify-between items-center border-t'>
+            <div className='w-1 h-full bg-green-300'></div>
+            <p className='flex-1 px-2 text-sm'>Estudar React</p>
+            <div className='flex gap-2 items-center'>
+              <SquarePen size={16} className='cursor-pointer' />
+              <Trash size={16} className='cursor-pointer' />
+            </div>
+          </div>
+
         </div>
+
+        <div className='flex justify-between'>
+          <div className='flex gap-2 items-center'>
+            <ListChecks size={18}/>
+            <p className='text-xs'>Tarefas Concluídas (3/3)</p>
+          </div>
+          <Button className='text-xs h-7 mt-4 cursor-pointer' variant='outline'><Trash />Limpar Tarefas Concluídas</Button>
+        </div>
+
+        <div className='h-2 w-full bg-gray-100 mt-4 rounded-md'>
+          <div className='h-full w-[50%] bg-blue-500 rounded-md' style={{width:'50%'}}></div>
+        </div>
+
+        <div className='flex justify-end items-center mt-2 gap-2'>
+          <Sigma size={18}/>
+          <p className='text-xs'>3 Tarefas no Total</p>
+        </div>
+
+        </CardContent>
 
       </Card>
     </main>
